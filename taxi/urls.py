9b4641b3c59ca7e2aaf4e28d4d809b80b6cb7933
Vasyl Perehinets <perehinets95@gmail.com>
2026-09-23 22:47:35 +0100

    solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -65,7 +65,7 @@ urlpatterns = [
         name="driver-update"
     ),
     path(
-        "drivers/<int:pk>/delete",
+        "drivers/<int:pk>/delete/",
         DriverDeleteView.as_view(),
         name="driver-delete"
     ),
